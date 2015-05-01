69fac2afbe26a9885c29158b61a1e4cb056deedf
Nathan Faucett <nathanfaucett@gmail.com>
2015-05-01 12:18:00 -0500

    updates

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 var reInflect = require("re_inflect");
 
 
-module.exports = function constantize(string) {
+module.exports = constantize;
+
+
+function constantize(string) {
     return string.match(reInflect).join("_").toUpperCase();
-};
+}
